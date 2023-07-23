2ce7456f6f1f19f7cb78c813e14fc560c26f8141
Moshe Lev - Ari <8668871@gmail.com>
2023-07-23 09:34:29 +0300

    added readme

--- src/Medaware/src/Main.java
+++ src/Medaware/src/Main.java
@@ -8,15 +8,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String inputFilePath = "Medaware/data/filtered_output.csv";
+        String inputFilePath = "data/filtered_output.csv";
         String outputFilePath = "result.txt";
         SentenceAnalyzer sentenceAnalyzer = new SentenceAnalyzer(inputFilePath, outputFilePath);
         sentenceAnalyzer.run();
 
-        try {
-            sleep(100000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
